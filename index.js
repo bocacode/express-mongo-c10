6f54e05571bbebe5e131e9e8b8b2455c31403335
Todd H. Albert <todd@bocacode.com>
2023-04-05 11:44:28 -0400

    adding and getting plants

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { getAllPlants, addPlant } from "./src/plants.js";
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/plants", getAllPlants);
+app.post("/plants", addPlant);
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}...`);
+});
